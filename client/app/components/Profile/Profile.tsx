e9b795ff237bfdd43c603aae1c50c5533465aa84
rinkupuri <rinkupuri124@gmail.com>
2024-03-15 00:46:03 +0530

    Profile Page Side Bar Complete with responsive in Mobile and Pc Both

--- client/app/components/Profile/Profile.tsx
+++ client/app/components/Profile/Profile.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import SideBar from "./SideBar";
 import { useSelector } from "react-redux";
+import avatarImage from "../../../public/user (1).png";
 
 type Props = {};
 
 const Profile = (props: Props) => {
   const { user } = useSelector((state: any) => state.auth);
-  const [avatar, setAvatar] = useState(
-    "https://img.freepik.com/free-vector/isolated-young-handsome-man-different-poses-white-background-illustration_632498-859.jpg?t=st=1710358314~exp=1710361914~hmac=1326ed069883aac900b23adcf37a95cde14a3ce04dbb8a796c85a5f734c6ee7b"
-  );
+  const [avatar, setAvatar] = useState(avatarImage);
   const [active, setActive] = useState(0);
   return (
     <div className="flex w-full h-screen justify-center mt-5 items-center">
--- client/app/components/Profile/SideBar.tsx
+++ client/app/components/Profile/SideBar.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import { style } from "@/app/styles/style";
+import Image, { StaticImageData } from "next/image";
 import React, { FC } from "react";
+import { BiLogOut } from "react-icons/bi";
+import { FaFileInvoice } from "react-icons/fa";
+import { MdPayment } from "react-icons/md";
+import { SiCoursera } from "react-icons/si";
 
 type Props = {
-  avatar: string;
-  setAvatar: (avatar: string) => void;
+  avatar: StaticImageData;
+  setAvatar: (avatar: StaticImageData) => void;
   user: {
     name: string;
     email: string;
@@ -17,16 +22,68 @@ type Props = {
 
 const SideBar: FC<Props> = ({ avatar, setAvatar, user, setActive, active }) => {
   return (
-    <div className="flex-[1.5] w-full items-center">
-      <div className="flex w-10/12 h-fit flex-col">
+    <div className="flex-[1.5]  w-full  justify-center items-center">
+      <div className="flex w-[80px] md:w-10/12 xl:w-8/12 h-[80%] items-center rounded-md shadow-md border-[1px] dark:border-slate-800 flex-col">
         <Image
-          width={150}
-          height={150}
-          className="w-[150px] h-[150px] object-cover"
+          width={80}
+          height={80}
+          className="mt-5 w-10 h-10 800px:w-[90px] 800px:h-[90px] object-cover"
           alt="Profile Image"
-          src={`${avatar}`}
+          src={user?.avatar?.url ? user.avatar.url : avatar}
         />
-        <h1>{user.name}</h1>
+        <h1 className={`${style.title}`}>{user.name}</h1>
+        <div
+          onClick={() => setActive(0)}
+          className={`flex cursor-pointer ${
+            active === 0 ? "dark:bg-slate-900" : ""
+          } justify-center mt-2 h-[60px] items-center w-full`}
+        >
+          <div
+            className={`flex justify-center  md:justify-start gap-3  items-center w-10/12 `}
+          >
+            <SiCoursera size={20} className="text-black dark:text-white" />
+            <h1 className="md:block hidden">Couses</h1>
+          </div>
+        </div>
+        <div
+          onClick={() => setActive(1)}
+          className={`flex cursor-pointer ${
+            active === 1 ? "dark:bg-slate-900" : ""
+          } justify-center mt-2 h-[60px] items-center w-full`}
+        >
+          <div
+            className={`flex justify-center  md:justify-start gap-3  items-center w-10/12 `}
+          >
+            <MdPayment size={20} className="text-black dark:text-white" />
+            <h1 className="md:block hidden">Saved Cards</h1>
+          </div>
+        </div>
+        <div
+          onClick={() => setActive(2)}
+          className={`flex cursor-pointer ${
+            active === 2 ? "dark:bg-slate-900" : ""
+          } justify-center mt-2 h-[60px] items-center w-full`}
+        >
+          <div
+            className={`flex justify-center  md:justify-start gap-3  items-center w-10/12 `}
+          >
+            <FaFileInvoice size={20} className="text-black dark:text-white" />
+            <h1 className="md:block hidden">Payment History</h1>
+          </div>
+        </div>
+        <div
+          onClick={() => setActive(3)}
+          className={`flex cursor-pointer ${
+            active === 3 ? "dark:bg-slate-900" : ""
+          } justify-center mt-2 h-[60px] items-center w-full`}
+        >
+          <div
+            className={`flex justify-center  md:justify-start gap-3  items-center w-10/12 `}
+          >
+            <BiLogOut size={20} className="text-black dark:text-white" />
+            <h1 className="md:block hidden">Log Out</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
